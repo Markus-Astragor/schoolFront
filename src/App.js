aae007b23e929e312a2194c5519d053ca572197a
Markus-Astragor <kosyk.markian@gmail.com>
2023-02-27 18:21:55 +0200

    Login_menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Login_and_register from './Components/Login_register/Login_and_register';
 import Main_page from './Components/Main_page/Main_page';
 
 function App() {
   return (
     <div className="App">
       <Main_page/>
-      <div className='test'></div>
+      <Login_and_register/>
     </div>
   );
 }
---
+++ src/Components/Login_register/Login_and_register.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Login_and_register.css';
+
+function Login_and_register() {
+    return (
+        <div className="Login_and_register">
+            <div className="Forms">
+                <div className="buttons_autorization">
+                    <div className="Login">Увійти</div>
+                    <div className="Register">Зареєструватися</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login_and_register;
