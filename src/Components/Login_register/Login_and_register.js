9486e094f9b9018e085b99169adb4d58fdb55562
Marko Kosyk <kosyk.markian@gmail.com>
2023-03-04 21:02:34 +0200

    new_changes_fix_bugs

--- src/Components/Login_register/Login_and_register.js
+++ src/Components/Login_register/Login_and_register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react"
 import { NavLink } from 'react-router-dom';
 import Login from "./Login";
 import Register from './Register';
@@ -6,13 +7,26 @@ import './Login_and_register.css';
 import { Route, Routes } from 'react-router-dom';
 
 
+
 function Login_and_register() {
+    const [greenBtn, setGreenBtn] = useState(true);
+    const [greenRegisterBtn, setGreenRegisterBtn] = useState(false);
+
+    const greenLoginBtnChange = () =>{
+        setGreenRegisterBtn(false)
+        setGreenBtn(true);
+    }
+
+    const greenRegisterBtnChange = () => {
+        setGreenBtn(false);
+        setGreenRegisterBtn(true);
+    }
     return (
         <div className="Login_and_register">
             <div className="Forms">
                 <div className="buttons_autorization">
-                    <div className="Login_btn"><NavLink to='/' style={{ textDecoration: 'none', color: 'black' }}>Увійти</NavLink></div>
-                    <div className="Register"><NavLink to='/register' style={{ textDecoration: 'none', color: 'black' }}>Зареєструватися</NavLink></div>
+                    <div className={greenBtn ? 'Login_btn' : 'Login_btn_no_color'}><NavLink to='/' style={{ textDecoration: 'none', color: 'black', width: '100%', display: 'block' }} onClick={greenLoginBtnChange}>Увійти</NavLink></div>
+                    <div className={greenRegisterBtn ? 'Register' : 'Register_btn_no_color'}><NavLink to='/register' style={{ textDecoration: 'none', color: 'black', width: '100%', display: 'block'}} onClick={greenRegisterBtnChange}>Зареєструватися</NavLink></div>
                 </div>
                 <hr />
                 <Routes>
