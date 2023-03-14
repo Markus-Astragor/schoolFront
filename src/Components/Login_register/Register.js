ed1cb3ca9d03ab255c2c7b99cb4dbda5a937c7e8
Markus-Astragor <kosyk.markian@gmail.com>
2023-03-14 14:14:24 +0200

    register start

--- src/Components/Login_register/Register.js
+++ src/Components/Login_register/Register.js
@@ -1,6 +1,49 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 
 function Register() {
+  const [signUpData, setSignUpData] = useState({
+      email: "",
+      password: "",
+      person: "student",
+      username: "Ostap"
+  })
+  const emailInputRef = useRef(null);
+  const passwordRef = useRef(null);
+  const repeatPasswordRef = useRef(null);
+
+  
+
+  function onSubmit(e){
+
+    e.preventDefault();
+    
+    const email = emailInputRef.current.value;
+    const password = passwordRef.current.value;
+    
+
+    
+    setSignUpData({
+      email: email,
+      password: password
+    })
+
+    console.log(signUpData);
+
+    
+
+
+    axios.post('http://localhost:3000/register', signUpData)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+ 
+
   return (
     <div className='Login'>
       <div className="Login_as_pupil">
@@ -8,11 +51,11 @@ function Register() {
         <hr />
       </div>
       <form className='Form_Login'>
-        <input required type='email' placeholder='Email'></input>
-        <input required type='password' placeholder='Password'></input>
-        <input required type='password' placeholder='Password'></input>
+        <input required type='email' placeholder='Email' ref={emailInputRef}></input>
+        <input required type='password' placeholder='Password' ref={passwordRef}></input>
+        <input required type='password' placeholder='Repeat password' ref={repeatPasswordRef}></input>
         <div className='div_submit_btn'>
-        <button className='submit_btn'>Зареєструватися</button>
+        <button className='submit_btn' onClick={(e)=>{onSubmit(e)}}>Зареєструватися</button>
         </div>
       </form>
     </div>
