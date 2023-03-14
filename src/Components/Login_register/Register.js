ed8ab0dc9155d66f45e78f9a80d05bf9de24773d
Marko Kosyk <kosyk.markian@gmail.com>
2023-03-15 00:48:23 +0200

    dropdown

--- src/Components/Login_register/Register.js
+++ src/Components/Login_register/Register.js
@@ -1,14 +1,15 @@
 import React, { useRef, useState } from 'react';
+import RegisterStyle from  './Register.module.css';
 import axios from 'axios';
 
 function Register() {
 
 
   const [signUpData, setSignUpData] = useState({
-      email: "",
-      password: "",
-      person: "student",
-      username: "Ostap"
+    email: "",
+    password: "",
+    person: "student",
+    username: "Ostap"
   })
 
 
@@ -16,17 +17,17 @@ function Register() {
   const passwordRef = useRef(null);
   const repeatPasswordRef = useRef(null);
 
-  
 
-  function onSubmit(e){
+
+  function onSubmit(e) {
 
     e.preventDefault();
-    
+
     const emailText = emailInputRef.current.value;
     const passwordText = passwordRef.current.value;
     const personText = 'student';   //Замінити на значення person
     const usernametext = 'Ostap'; // витягнути від юзера
-    
+
 
 
     axios.post('http://localhost:8080/register', {
@@ -35,20 +36,28 @@ function Register() {
       email: emailText,
       person: personText
     })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
- 
+
 
   return (
     <div className='Login'>
-      <div className="Login_as_pupil">
-        <h3>Зареєструватися як учень</h3>
+      <div className={RegisterStyle.RegisterWrapper}>
+        <h3 style={{margin: '10px 0px 15px 0px'}}>Зареєструватися як:</h3>
+        <div className={RegisterStyle.select}>
+        <i></i>
+        <select option='dropdown'>
+       
+          <option value='01'>Вчитель</option>
+          <option value='01'>Учень</option>
+        </select>
+        </div>
         <hr />
       </div>
       <form className='Form_Login'>
@@ -56,7 +65,7 @@ function Register() {
         <input required type='password' placeholder='Password' ref={passwordRef}></input>
         <input required type='password' placeholder='Repeat password' ref={repeatPasswordRef}></input>
         <div className='div_submit_btn'>
-        <button className='submit_btn' onClick={(e)=>{onSubmit(e)}}>Зареєструватися</button>
+          <button className='submit_btn' onClick={(e) => { onSubmit(e) }}>Зареєструватися</button>
         </div>
       </form>
     </div>
