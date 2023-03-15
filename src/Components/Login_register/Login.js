8dd0ed635938c4744bc514933813f0808c29dd2a
Sidoryak-Ostap <ostapsidorak@gmail.com>
2023-03-15 16:58:51 +0200

    15/03

--- src/Components/Login_register/Login.js
+++ src/Components/Login_register/Login.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import './Login.css';
+import { useState, useRef } from 'react';
+
+import axios from 'axios';
+
+
 
 function Login() {
+
+
+  
+  const SubmitLogin = (e) =>{
+      e.preventDefault();
+
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+
+
+      axios.post('http://localhost:8080/login', {
+        email: email,
+        password: password
+      }).then((response) =>{
+        console.log(response);
+      }).catch(err =>{
+        console.log(err);
+      })
+  }
+
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
   return (
+
     <div className='Login'>
       <div className="Login_as_pupil">
         <h3>Увійти як учень</h3>
         <hr />
       </div>
       <form className='Form_Login'>
-        <input required type='email' placeholder='Email'></input>
-        <input required type='password' placeholder='Password'></input>
+        <input ref={emailRef} required type='email' placeholder='Email'></input>
+        <input ref={passwordRef} required type='password' placeholder='Password'></input>
         <div className='div_submit_btn'>
-          <button className='submit_btn'>Увійти</button>
+          <button onClick={(e)=>{SubmitLogin(e)}} className='submit_btn'>Увійти</button>
         </div>
       </form>
     </div>
--- src/Components/Login_register/Register.js
+++ src/Components/Login_register/Register.js
@@ -5,17 +5,11 @@ import axios from 'axios';
 function Register() {
 
 
-  const [signUpData, setSignUpData] = useState({
-    email: "",
-    password: "",
-    person: "student",
-    username: "Ostap"
-  })
-
-
   const emailInputRef = useRef(null);
   const passwordRef = useRef(null);
   const repeatPasswordRef = useRef(null);
+  const PerosnRef = useRef();
+  const UsernameRef = useRef();
 
 
 
@@ -25,24 +19,50 @@ function Register() {
 
     const emailText = emailInputRef.current.value;
     const passwordText = passwordRef.current.value;
-    const personText = 'student';   //Замінити на значення person
-    const usernametext = 'Ostap'; // витягнути від юзера
+    const personText = PerosnRef.current.value;
+    const usernametext = UsernameRef.current.value;
+    const repeatPasswordText = repeatPasswordRef.current.value;
+
+    const RegisterValidation  = CheckUserValid(emailText, passwordText, repeatPasswordText, personText, usernametext);
+
+    if(RegisterValidation == 1)
+    {
+      return;
+    }
 
 
 
     axios.post('http://localhost:8080/register', {
+
       password: passwordText,
       username: usernametext,
       email: emailText,
       person: personText
-    })
-      .then(function (response) {
+
+    }).then(function (response) {
         console.log(response);
-      })
-      .catch(function (error) {
+      }).catch(function (error) {
         console.log(error);
       });
   }
+
+  function CheckUserValid(email, password, repeatPassword, person, username)
+  {
+    if(!(password == repeatPassword))
+    {
+      console.log('password is different ');
+      return 1;
+    }
+
+    if(username.length < 3)
+    {
+      console.log('username should consisit of 3 letter minimum');
+      return 1;
+    }
+
+  }
+
+
 
 
 
@@ -52,16 +72,17 @@ function Register() {
         <h3 style={{margin: '10px 0px 15px 0px'}}>Зареєструватися як:</h3>
         <div className={RegisterStyle.select}>
         <i></i>
-        <select option='dropdown'>
+        <select ref={PerosnRef} option='dropdown'>
        
-          <option value='01'>Вчитель</option>
-          <option value='01'>Учень</option>
+          <option value='teacher'>Вчитель</option>
+          <option value='student'>Учень</option>
         </select>
         </div>
         <hr />
       </div>
       <form className='Form_Login'>
         <input required type='email' placeholder='Email' ref={emailInputRef}></input>
+        <input required type='username' placeholder='Name' ref={UsernameRef}></input>
         <input required type='password' placeholder='Password' ref={passwordRef}></input>
         <input required type='password' placeholder='Repeat password' ref={repeatPasswordRef}></input>
         <div className='div_submit_btn'>
