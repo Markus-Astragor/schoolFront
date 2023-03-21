1980d510d00a3c544055dd92765dd0bc71b234e4
DESKTOP-378FDDD\Markus <kosyk.markian@gmail.com>
2023-03-21 11:20:18 +0200

    new fixes

--- src/Components/Video_block/index.js
+++ src/Components/Video_block/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './style.css';
 import Slider from "react-slick";
 
@@ -35,6 +35,10 @@ function SamplePrevArrow(props) {
   
 
 function Video_block(){
+  let bloggerList = useRef(null);
+  function openList(){
+    bloggerList.current.style.display = 'block';
+  }
   
     
     const settings = {
@@ -86,8 +90,8 @@ function Video_block(){
               <div className='white_info_bloger_block'>
                 <h2>Блогери що допоможуть тобі з навчанням</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop</p>
-                <div className='greenButtom_VideoBlock'>відкрити список</div>
-                <div className='list_of_bloger'>
+                <div className='greenButtom_VideoBlock' onClick={openList}>відкрити список</div>
+                <div className='list_of_bloger' ref={bloggerList}>
                   <ul>
                   <li>Lorem ipsum</li>
                   <li>Lorem ipsum</li>
