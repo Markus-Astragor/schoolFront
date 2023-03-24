5b2743d1a8e73feff7b28d50237d31319a97a5de
Sidoryak-Ostap <ostapsidorak@gmail.com>
2023-03-24 12:50:36 +0200

    local stirage changes

--- src/Components/Login_register/Login_and_register.js
+++ src/Components/Login_register/Login_and_register.js
@@ -6,13 +6,14 @@ import Register from './Register';
 import './Login_and_register.css';
 import { Route, Routes } from 'react-router-dom';
 
-
+import closePopUp from '../../images/Log_register_images/cancel.png';
 
 
 function Login_and_register(props) {
     const [greenBtn, setGreenBtn] = useState(true);
     const [greenRegisterBtn, setGreenRegisterBtn] = useState(false);
     const [register,setRegister] = useState(false);
+    const [invalidUser, setInvalidUser] = useState(false);
 
     const greenLoginBtnChange = () => {
         setGreenRegisterBtn(false)
@@ -21,6 +22,7 @@ function Login_and_register(props) {
     }
 
     const greenRegisterBtnChange = () => {
+        setInvalidUser(false);
         setGreenBtn(false);
         setGreenRegisterBtn(true);
         setRegister(true);
@@ -32,6 +34,7 @@ function Login_and_register(props) {
 
     return (
         <div ref = {LoginRef} className="Login_and_register">
+
             <div className="Forms">
                 <div className="buttons_autorization">
                     <div className={greenBtn ? 'Login_btn' : 'Login_btn_no_color'}><NavLink to='/authoriztion' style={{ textDecoration: 'none', color: 'black', width: '100%', display: 'block' }} onClick={greenLoginBtnChange}>Увійти</NavLink></div>
@@ -41,10 +44,13 @@ function Login_and_register(props) {
 
 
                {
-                register ? <Register/> : <Login/>
+                register ? <Register/> : <Login setInvalidUser={setInvalidUser}/>
                }
 
+            </div>
 
+            <div className={invalidUser? 'show_auth_error': 'auth_eror'}>
+                Неправильний email або пароль <img className="popUp_close" onClick={()=>{setInvalidUser(false)}} src={closePopUp}/>
             </div>
         </div>
     )
